320b16cf5aa91f1aa6c186672391e0ca501a3bcc
Rafal Niesler <rafal.niesler@cern.ch>
2016-03-26 00:08:19 +0100

    java8

--- java8/src/rniesler/FileRename.java
+++ java8/src/rniesler/FileRename.java
@@ -1,8 +1,51 @@
 package rniesler;
 
-class FileRename {
+import java.io.IOException;
+import java.nio.file.*;
 
-    public static void main (String[] args) {
-        System.out.println("Hello world");
+class FileRename {
+    private String dirPath;
+
+    protected FileRename(String dirPath) {
+        this.dirPath = dirPath;
+    }
+
+    public void doRename() {
+        try {
+            Files.list(Paths.get(dirPath)).forEach((Path fromPath) -> {
+                        Path toPath = convertPath(fromPath);
+                        System.out.println("Renaming: " + fromPath + " to " + toPath);
+                        try {
+                            Files.move(fromPath, toPath);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+            );
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private Path convertPath(Path fromPath) {
+        String fileName=fromPath.getFileName().toString();
+        if(Character.isUpperCase(fileName.charAt(0))) {
+            fileName = fileName.toLowerCase();
+        } else {
+            fileName = fileName.toUpperCase();
+        }
+        Path toPath;
+        if(fromPath.getParent() != null) {
+            toPath = fromPath.getParent().resolve(fileName);
+        } else {
+            toPath = Paths.get(fileName);
+        }
+        return toPath;
+    }
+
+    public static void main(String[] args) {
+        new FileRename("test").doRename();
     }
 }
