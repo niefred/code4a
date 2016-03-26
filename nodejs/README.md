9afd149063a9b3d3de8e624f5e5d169d3a2f8a68
rniesler <rniesler@gmail.com>
2016-03-26 08:31:46 -0400

    nodejs

---
+++ nodejs/README.md
@@ -0,0 +1,10 @@
+Uruchamianie:
+node main.js
+
+Zasoby:
+* http://www.w3schools.com/js/
+* http://www.tutorialspoint.com/nodejs/index.htm
+* http://www.tutorialspoint.com/nodejs_terminal_online.php
+* https://git-scm.com/book/en/v2
+* http://www.w3schools.com/jsref/default.asp
+* https://nodejs.org/api/
---
+++ nodejs/main.js
@@ -0,0 +1,28 @@
+var dir = 'test'
+var fs = require('fs')
+fs.readdir(dir, function(err,fileNames) {
+  if(err) {
+    console.error('Could not read '+dir+': '+err)
+    return
+  }
+  fileNames.forEach(function(fileName) {
+    var oldPath = dir + '/' + fileName
+    var newPath = dir + '/' + convert(fileName)
+    //console.log('Renaming: '+oldPath+' to '+newPath)
+    fs.rename(oldPath, newPath, function(err) {
+      if(err) {
+        console.log('Could not rename '+oldPath+' to '+newPath)
+      } else {
+        console.log('Renamed '+oldPath+' to '+newPath)
+      }
+    })
+  })
+})
+
+function convert(fileName) {
+  if(fileName.charAt(0)==fileName.charAt(0).toUpperCase()) {
+    return fileName.toLowerCase()
+  } else {
+    return fileName.toUpperCase()
+  }
+}
